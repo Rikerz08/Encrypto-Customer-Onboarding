f344ae61f6859a5af89b077b6b925fccc2c391cb
Rikerz08 <rikikarl8@gmail.com>
2024-07-23 00:55:15 +0800

    init

---
+++ encrypto-customer-onboarding/src/main/kotlin/com/example/encrypto_customer_onboarding/EncryptoCustomerOnboardingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.encrypto_customer_onboarding
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EncryptoCustomerOnboardingApplication
+
+fun main(args: Array<String>) {
+	runApplication<EncryptoCustomerOnboardingApplication>(*args)
+}
---
+++ encrypto-customer-onboarding/src/test/kotlin/com/example/encrypto_customer_onboarding/EncryptoCustomerOnboardingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.encrypto_customer_onboarding
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EncryptoCustomerOnboardingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
